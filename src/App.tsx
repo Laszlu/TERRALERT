dd9b67f3dc4dfa7485b2b5cd30e5569c0882723f
Laszlu <maurice.ferreyra@gmail.com>
2023-10-07 15:26:40 +0200

    moved globe to center of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 import TestGlobe from './globe.tsx';
 
 function App() {
--- src/globe.tsx
+++ src/globe.tsx
@@ -6,7 +6,9 @@ function globe(){
         <>
             <Globe
             globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+            // backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
             backgroundColor='#000000'
+            width={1200}
             />
         </>
     )
