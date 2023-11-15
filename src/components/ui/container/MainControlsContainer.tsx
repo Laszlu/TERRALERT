1e2e84490d3030d15afc5e4bcf0ffc17b38112ad
Laszlu <maurice.ferreyra@gmail.com>
2023-11-15 16:19:46 +0100

    formatting

--- src/components/ui/container/MainControlsContainer.tsx
+++ src/components/ui/container/MainControlsContainer.tsx
@@ -1,40 +1,54 @@
 import { ToggleButton } from "../buttons/ButtonsIndex";
 import { ControlsHeader } from "../headers/HeaderIndex";
-import { useFetchHook } from '../../../utilities/UtilitiesIndex';
-import '../../../assets/styles/container.css';
+import { useFetchHook } from "../../../utilities/UtilitiesIndex";
+import "../../../assets/styles/container.css";
 
 type ContainerProps = {
-  height: number;
-  width: number;
-}
+	height: number;
+	width: number;
+};
 
-function testButtonClick(){
-  console.log("button clicked");
+function testButtonClick() {
+	console.log("button clicked");
 }
 
 function mainControlsContainter(props: ContainerProps) {
+	const stormPilar = useFetchHook(
+		"https://eonet.gsfc.nasa.gov/api/v2.1/events/EONET_6447"
+	);
 
-  const stormPilar = useFetchHook("https://eonet.gsfc.nasa.gov/api/v2.1/events/EONET_6447");
+	//const {data} = useFetchHook("https://jsonplaceholder.typicode.com/users");
 
-  //const {data} = useFetchHook("https://jsonplaceholder.typicode.com/users");
+	function testApiFetch() {
+		console.log(stormPilar);
+	}
 
-  function testApiFetch(){
-    console.log(stormPilar);
-  }
-
-  return (
-    <div className="main-controls-container">
-      <ControlsHeader headerText="Controls"/>
-      <div className="button-container first-row-buttons">
-        <ToggleButton buttonText="Button1" click={testButtonClick}/>
-        <ToggleButton buttonText="Button2" click={testApiFetch}/>
-      </div>
-      <div className="button-container second-row-buttons">
-        <ToggleButton buttonText="Button3" click={testApiFetch}/>
-        <ToggleButton buttonText="Button4" click={testApiFetch}/>
-      </div>
-    </div>
-  );
+	return (
+		<div className="main-controls-container">
+			<ControlsHeader headerText="Controls" />
+			<div className="button-container first-row-buttons">
+				<ToggleButton
+					buttonText="Button1"
+					click={testButtonClick}
+				/>
+				<ToggleButton
+					buttonText="Button2"
+					click={testApiFetch}
+				/>
+			</div>
+			<div className="button-container second-row-buttons">
+				<ToggleButton
+					buttonText="Button3"
+					click={testApiFetch}
+				/>
+				<ToggleButton
+					buttonText="Button4"
+					click={testApiFetch}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default mainControlsContainter;
+
